cab985629a36b26d9e78634d3d1cbf156fa19e4a
wtaype <wtaype@users.noreply.github.com>
2026-01-21 03:24:12 +0000

    deploy: e8ad58aafaebabebfa91b67280c757da6905d6bf

---
+++ v12/assets/smile-vKZK3t28.js
@@ -0,0 +1,21 @@
+import"./vendor-gzd0YkcT.js";import{g as i,S as s}from"./main-BbX5aLDu.js";import"./main-DuvwKID_.js";let a=i("wiSmile");const r=async()=>`
+   <div class="miweb">
+    <div class="mhead">
+     <div class="mhead_left">
+      <h1 class="mh1"><i class="fas fa-trophy"></i> ${s()} ${a.nombre}</h1>
+      <ul class="mfeatures">
+       <li><i class="fas fa-check-circle"></i> Personaliza las zonas horarias</li>
+       <li><i class="fas fa-check-circle"></i> Personaliza el calendario</li>
+       <li><i class="fas fa-check-circle"></i> Sincroniza en tiempo real</li>
+      </ul>
+     </div>
+     <div class="mhead_right">
+      <button class="btn wfresh">Actualizar <i class="fa-solid fa-rotate-right"></i></button>
+     </div>
+    </div>
+    <div class="mibody">
+     <div class="wiHoras"></div>
+     <div class="wiMeses"></div>
+    </div>
+   </div>
+  `,o=async()=>{},t=()=>{console.log("😊 Smile limpiado")};export{t as cleanup,o as init,r as render,a as smile};
